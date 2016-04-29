f6a7bb670a3b94825781b7beb9dc0848045bebbb
Philipp von dem Bussche <philipp.bussche@appdynamics.com>
2016-04-29 11:28:22 +0200

    in the delta function using abs() now to always report the absolute number and no negative numbers (since reporting a negative number to the Controller would not work).

--- src/main/groovy/com/appdynamics/ace/agents/derivedMetrics/groovy/CalculationDelegate.groovy
+++ src/main/groovy/com/appdynamics/ace/agents/derivedMetrics/groovy/CalculationDelegate.groovy
@@ -139,7 +139,7 @@ class CalculationDelegate extends Script {
         def values = this.getValues(metricName);
         if(values.size()>1) {
             getLogger().debug("Last value is " + values.last() + ", second last value is " + values[-2] + ". Hence the delta is " + values.last() - values[-2]);
-            return values.last() - values[-2];
+            return (values.last() - values[-2]).abs();
         } else {
            throw new CalculationException("There are less than two values to derive a delta from.");
             return null;
@@ -271,7 +271,7 @@ class CalculationDelegate extends Script {
                      String timeRollup = AVERAGE,
                      String cluster = INDIVIDUAL) {
         logger.info("report metric $path : $value");
-        def metricValue = new MetricValueContainer ( path, (long)value,
+        def metricValue = new MetricValueContainer ( path, (long) value,
                         aggregation,timeRollup,cluster) ;
 
         def values = _metricValues[path] ?: []
